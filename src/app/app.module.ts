0866d7e034d41df2fa11a9136adaeecec388969c
timroe7 <timothy.roe@hope.edu>
2019-09-28 12:08:17 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { CounterComponent } from './counter/counter.component';
 
@@ -11,6 +10,6 @@ import { CounterComponent } from './counter/counter.component';
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, CounterComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [ MessageService]
+  providers: [ ]
 })
 export class AppModule { }
