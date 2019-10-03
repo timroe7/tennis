145999ecdd1383b0835a86da2a53a4abf7b35ccd
timroe7 <timothy.roe@hope.edu>
2019-10-02 21:11:35 -0400

    Added new classes for game, set match

---
+++ src/app/game.ts
@@ -0,0 +1,6 @@
+export class Game {
+  AllPoints: Array<boolean>;
+  Team1Points: number;
+  Team2Points: number;
+  WithAd: boolean;
+}
---
+++ src/app/set.ts
@@ -0,0 +1,9 @@
+import { Game } from './game';
+
+export class Set {
+  AllGames: Array<Game>;
+  Team1Games: number;
+  Team2Games: number;
+  NumberOfGamesToWinSet: number;
+  PlayTiebreakAt: number;
+}
