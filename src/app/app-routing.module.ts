bf635fee8a3f0eff25885b39a40c9e75ee670208
unknown <jimmy.cerone@gmail.com>
2019-10-16 17:03:15 -0400

    Files necessary for running locally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/counter', pathMatch: 'full' },
+  { path: '', redirectTo: '/home-page', pathMatch: 'full' },
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'counter', redirectTo: '/counter', pathMatch: 'full'},
   { path: 'counter', component: CounterComponent }
 ];
 
