3ca337591465067214f0d524a3dc3d4dd599c2ad
timroe7 <timothy.roe@hope.edu>
2019-10-21 22:53:12 -0400

    2 files modified

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,64 +1,100 @@
 export class Game {
   AllPoints = new Array<boolean>();
   Team1Points = 0;
-  Team1PointsDisplay = '0';
+  Team1PointsDisplay = "0";
   Team1TiebreakScore = 0;
   Team2Points = 0;
-  Team2PointsDisplay = '0';
+  Team2PointsDisplay = "0";
   Team2TiebreakScore = 0;
   WithAd = true;
   isFinished = false;
   gameWonBy = false;
   IsTiebreak = false;
 
-  public Team1Point()  {
+  Team1Point() {
     if (!this.isFinished) {
-    this.Team1PointsDisplay = this.UpdatePointsDisplay(this.Team1PointsDisplay, this.Team2PointsDisplay);
-      this.AllPoints.push(true);
+        this.Team1PointsDisplay = this.UpdatePointsDisplay(
+          this.Team1PointsDisplay,
+          this.Team2PointsDisplay
+        );
+        this.AllPoints.push(true);
+      
     }
     if (this.isFinished) {
       this.gameWonBy = true;
     }
   }
-  public Team2Point() {
+  Team2Point() {
     if (!this.isFinished) {
-    this.Team2PointsDisplay = this.UpdatePointsDisplay(this.Team2PointsDisplay, this.Team1PointsDisplay);
-      this.AllPoints.push(false);
+        this.Team2PointsDisplay = this.UpdatePointsDisplay(
+          this.Team2PointsDisplay,
+          this.Team1PointsDisplay
+        );
+        this.AllPoints.push(false);
+      
     }
     if (this.isFinished) {
       this.gameWonBy = false;
     }
   }
 
-  public UpdatePointsDisplay(teamPointWon: string, teamPointLost: string): string {
-    if (teamPointWon == '0') {
-      return '15';
-    }
-    else if (teamPointWon == '15') {
-      return '30';
-    }
-    else if (teamPointWon == '30') {
-      return '40';
-    }
-    else if (teamPointWon == '40' && (teamPointLost == '30' || teamPointLost == '15' || teamPointLost == '0')) {
+  UpdatePointsDisplay(teamPointWon: string, teamPointLost: string): string {
+    if (teamPointWon == "0") {
+      return "15";
+    } else if (teamPointWon == "15") {
+      return "30";
+    } else if (teamPointWon == "30") {
+      return "40";
+    } else if (
+      teamPointWon == "40" &&
+      (teamPointLost == "30" || teamPointLost == "15" || teamPointLost == "0")
+    ) {
       this.isFinished = true;
-      this.Team1PointsDisplay = '0';
-      this.Team2PointsDisplay = '0';
-      return '0';
-    }
-    else if (teamPointWon == '40' && teamPointLost == '40') {
-      return 'Adv';
-    }
-    else if (teamPointWon == '40' && teamPointLost == 'Adv') {
-      this.Team1PointsDisplay = '40';
-      this.Team2PointsDisplay = '40';
-      return '40';
-    }
-    else if (teamPointWon == 'Adv') {
+      this.Team1PointsDisplay = "0";
+      this.Team2PointsDisplay = "0";
+      return "0";
+    } else if (teamPointWon == "40" && teamPointLost == "40") {
+      return "Adv";
+    } else if (teamPointWon == "40" && teamPointLost == "Adv") {
+      this.Team1PointsDisplay = "40";
+      this.Team2PointsDisplay = "40";
+      return "40";
+    } else if (teamPointWon == "Adv") {
       this.isFinished = true;
-      this.Team1PointsDisplay = '0';
-      this.Team2PointsDisplay = '0';
-      return '0';
+      this.Team1PointsDisplay = "0";
+      this.Team2PointsDisplay = "0";
+      return "0";
+    }
+  }
+
+  TiebreakTeam1Point() {
+    if (!this.isFinished) {
+      this.Team1TiebreakScore++;
+      this.AllPoints.push(true);
+      if (
+        this.Team1TiebreakScore >= 7 &&
+        this.Team1TiebreakScore >= this.Team2TiebreakScore + 2
+      ) {
+        this.isFinished = true;
+      }
+    }
+    if (this.isFinished) {
+      this.gameWonBy = true;
+    }
+  }
+  TiebreakTeam2Point() {
+    if (!this.isFinished) {
+      this.Team2TiebreakScore++;
+      this.AllPoints.push(false);
+      if (
+        this.Team2TiebreakScore >= 7 &&
+        this.Team2TiebreakScore >= this.Team1TiebreakScore + 2
+      ) {
+        this.isFinished = true;
+      }
+    }
+    if (this.isFinished) {
+      this.gameWonBy = false;
     }
   }
 }
--- src/app/set.ts
+++ src/app/set.ts
@@ -1,52 +1,68 @@
-import { Game } from './game';
+import { Game } from "./game";
 
 export class Set {
   WhichSet = 0;
   AllGames = new Array<Game>();
   CurrentGame = new Game();
-  Team1Games =  0;
+  Team1Games = 0;
   Team2Games = 0;
   NumberOfGamesToWinSet = 6;
   PlayTiebreakAt = 6;
   IsSetFinished = false;
   SetWonBy = false;
+  IsInTieBreak = false;
 
   Team1Point() {
-    this.CurrentGame.Team1Point();
-    if (this.CurrentGame.isFinished) {
-    this.AllGames.push(this.CurrentGame);
-    if (this.CurrentGame.gameWonBy == true) {
-      this.Team1Games++;
+    if (this.IsInTieBreak) {
+      this.CurrentGame.TiebreakTeam1Point();
+    } else {
+      this.CurrentGame.Team1Point();
     }
-    else {
-      this.Team2Games++;
-    }
-    this.CurrentGame = new Game();
-    }
-  this.CheckForSetFinished();
+      if (this.CurrentGame.isFinished) {
+        this.AllGames.push(this.CurrentGame);
+        if (this.CurrentGame.gameWonBy == true) {
+          this.Team1Games++;
+        } else {
+          this.Team2Games++;
+        }
+        this.CurrentGame = new Game();
+      }
+      this.CheckForSetFinished();
+      this.AreWeInTiebreak();
   }
   Team2Point() {
-    this.CurrentGame.Team2Point();
-    if (this.CurrentGame.isFinished) {
-    this.AllGames.push(this.CurrentGame);
-    if (this.CurrentGame.gameWonBy == true) {
-      this.Team1Games++;
+    if (this.IsInTieBreak) {
+      this.CurrentGame.TiebreakTeam2Point();
+    } else {
+      this.CurrentGame.Team2Point();
     }
-    else {
-      this.Team2Games++;
-    }
-    this.CurrentGame = new Game();
-    }
-  this.CheckForSetFinished();
+      if (this.CurrentGame.isFinished) {
+        this.AllGames.push(this.CurrentGame);
+        if (this.CurrentGame.gameWonBy == true) {
+          this.Team1Games++;
+        } else {
+          this.Team2Games++;
+        }
+        this.CurrentGame = new Game();
+      }
+      this.CheckForSetFinished();
+      this.AreWeInTiebreak();
   }
   CheckForSetFinished() {
-    if (this.Team1Games >= 6 && this.Team2Games <= 4) {
+    if ((this.Team1Games >= 6 && this.Team2Games <= 4) || this.Team1Games == 7) {
       this.IsSetFinished = true;
       this.SetWonBy = true;
-    }
-    else if (this.Team2Games >= 6 && this.Team1Games <= 4) {
+    } else if ((this.Team2Games >= 6 && this.Team1Games <= 4) || this.Team2Games == 7) {
       this.IsSetFinished = true;
       this.SetWonBy = false;
+    }
+  }
+  AreWeInTiebreak() {
+    if (
+      this.Team1Games == this.NumberOfGamesToWinSet &&
+      this.Team2Games == this.NumberOfGamesToWinSet
+    ) {
+      this.IsInTieBreak = true;
     }
   }
 }
